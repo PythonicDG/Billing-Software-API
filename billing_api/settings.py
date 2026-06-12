7dbf1635607e7fe78d63332230ebcb3bcb06509d
PythonicDG <dipakgaikwadmg@gmail.com>
2026-06-12 13:06:00 +0530

    fix: update STATIC_URL and MEDIA_URL to include leading slashes for correct URL resolution

--- billing_api/settings.py
+++ billing_api/settings.py
@@ -163,11 +163,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
